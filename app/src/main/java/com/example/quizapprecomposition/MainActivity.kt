7ea3ccdbb92c5c45cb0ef855facfdea6af8afbca
mlxs01 <108293242+mlxs01@users.noreply.github.com>
2024-09-22 17:37:11 -0400

    Created Quiz with Recomposition

--- app/src/main/java/com/example/quizapprecomposition/MainActivity.kt
+++ app/src/main/java/com/example/quizapprecomposition/MainActivity.kt
@@ -5,12 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
 import com.example.quizapprecomposition.ui.theme.QuizAppRecompositionTheme
